5321e6f4f882077f5b2152bfb71d84a41a5c1184
yam ferreira <yamferreira6@gmail.com>
2025-07-23 01:11:15 -0300

    chore: add githooks setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-"use client"
-
 import { Button } from "@/app/_components/ui/button"
 
 const Home = () => {
-  return <Button>test</Button>
+  return <Button className="bg-blue-500 text-white">test</Button>
 }
 
 export default Home
