f252a6176b1b1323ffdc87fb6aebc59f9cfe12b0
yam ferreira <yamferreira6@gmail.com>
2025-08-26 00:37:13 -0300

    feat: add BarbsershopItem component

---
+++ app/_components/ui/barbershop-item.tsx
@@ -0,0 +1,49 @@
+import { Badge } from "./badge"
+import { Barbershop } from "../../generated/prisma"
+import { Button } from "./button"
+import { Card, CardContent } from "./card"
+import Image from "next/image"
+import { StarIcon } from "lucide-react"
+
+interface BarbershopItemProps {
+  barbershop: Barbershop
+}
+
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  return (
+    <Card className="min-w-[167px] rounded-2xl">
+      <CardContent className="p-0 px-1 pt-0">
+        {/* image */}
+
+        <div className="relative h-[159px] w-full">
+          <Image
+            fill
+            className="rounded-2xl object-cover"
+            src={barbershop.imageUrl}
+            alt={barbershop.name}
+          />
+
+          <Badge
+            variant="secondary"
+            className="absolute top-2 left-2 space-x-1"
+          >
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="text-xs font-semibold">5,0</p>
+          </Badge>
+        </div>
+
+        {/* content */}
+
+        <div className="px-1 py-3">
+          <h3 className="truncate font-semibold">{barbershop.name}</h3>
+          <p className="truncate text-sm text-gray-500">{barbershop.address}</p>
+          <Button variant="secondary" className="mt-3 w-full">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default BarbershopItem
--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,22 @@ import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/ui/barbershop-item"
 
-const Home = () => {
+const Home = async () => {
+  const barbershop = await db.barbershop.findMany({})
+
   return (
     <div>
       {/* header */}
       <Header />
       <div className="p-5">
-          <h2 className="text-xl font-bold">Olá, Yam!</h2>
-          <p>Segunda-feira, 05 de agosto.</p>
+        <h2 className="text-xl font-bold">Olá, Yam!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
 
         {/* search input */}
-        <div className="flex items-center gap-8 mt-6">
+        <div className="mt-6 flex items-center gap-3">
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
@@ -25,27 +29,36 @@ const Home = () => {
         </div>
 
         {/* banner image */}
-        <div className="relative mt-6 h-[150px] w-full ">
-          <Image src="/banner.png" fill className="rounded-xl object-cover" alt="Banner" />
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Banner"
+          />
         </div>
 
-        <Card className="mt-6">
+        <h2 className="mt-6 mb-3 text-xs font-bold text-gray-400 uppercase">
+          Agendamentos
+        </h2>
+
+        {/* card with booking item */}
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* div left */}
             <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit" >Confirmado</Badge>
+              <Badge className="w-fit">Confirmado</Badge>
               <h3 className="font-semibold">Corte de Cabelo</h3>
 
               <div className="flex items-center gap-2">
                 <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png"/>
+                  <AvatarImage src="https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png" />
                 </Avatar>
                 <p className="text-sm">Barbearia do Luiz</p>
-
               </div>
             </div>
             {/* div right */}
-            <div className="flex flex-col items-center justify-center px-5 py-5 border-l">
+            <div className="flex flex-col items-center justify-center border-l px-5 py-5">
               <p className="text-sm">Agosto</p>
               <p className="text-2xl">05</p>
               <p className="text-sm">10:00</p>
@@ -53,6 +66,15 @@ const Home = () => {
           </CardContent>
         </Card>
 
+        <h2 className="mt-6 mb-3 text-xs font-bold text-gray-400 uppercase">
+          Recomendados
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershop.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
