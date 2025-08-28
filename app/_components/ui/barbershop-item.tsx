1233d9a216e0cea30fa761a40b8027b6c9194f61
yam ferreira <yamferreira6@gmail.com>
2025-08-28 00:11:48 -0300

    feat: add BarbsershopPage

--- app/_components/ui/barbershop-item.tsx
+++ app/_components/ui/barbershop-item.tsx
@@ -4,6 +4,7 @@ import { Button } from "./button"
 import { Card, CardContent } from "./card"
 import Image from "next/image"
 import { StarIcon } from "lucide-react"
+import Link from "next/link"
 
 interface BarbershopItemProps {
   barbershop: Barbershop
@@ -37,8 +38,8 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-500">{barbershop.address}</p>
-          <Button variant="secondary" className="mt-3 w-full">
-            Reservar
+          <Button variant="secondary" className="mt-3 w-full" asChild>
+            <Link href={`/barbershop/${barbershop.id}`}>Reservar</Link>
           </Button>
         </div>
       </CardContent>
---
+++ app/barbershop/[id]/page.tsx
@@ -0,0 +1,68 @@
+import { db } from "@/app/_lib/prisma"
+import { ChevronLeftIcon, MapIcon, MenuIcon, StarIcon } from "lucide-react"
+import Image from "next/image"
+import { Button } from "@/app/_components/ui/button"
+import Link from "next/link"
+
+interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
+
+const BarbershopPage = async ({ params }: BarbershopPageProps) => {
+  //chamar banco de dados
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+  })
+  return (
+    <div>
+      {/* Image */}
+      <div className="relative h-[250px] w-full">
+        <Image
+          src={barbershop?.imageUrl || "/placeholder.png"}
+          alt={barbershop?.name || "Barbearia"}
+          fill
+          className="object-cover"
+        />
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute top-4 left-4"
+          asChild
+        >
+          <Link href="/">
+            <ChevronLeftIcon />
+          </Link>
+        </Button>
+
+        <Button
+          size="icon"
+          variant="secondary"
+          className="absolute top-4 right-4"
+        >
+          <MenuIcon />
+        </Button>
+      </div>
+
+      <div className="border-b border-solid p-5">
+        <h1 className="mb-3 text-2xl font-bold">{barbershop?.name}</h1>
+        <div className="mb-2 flex items-center gap-1">
+          <MapIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop?.address}</p>
+        </div>
+
+        <div className="flex items-center gap-1">
+          <StarIcon className="text-primary fill-primary" size={18} />
+          <p className="text-sm">5,0 (499 avaliações)</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopPage
