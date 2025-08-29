24db363e7d314fb88aa23ca5a46e3cb248d837f8
yam ferreira <yamferreira6@gmail.com>
2025-08-28 22:23:30 -0300

    feat: about us

--- app/barbershop/[id]/page.tsx
+++ app/barbershop/[id]/page.tsx
@@ -3,6 +3,7 @@ import { ChevronLeftIcon, MapIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import { Button } from "@/app/_components/ui/button"
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 interface BarbershopPageProps {
   params: {
@@ -18,13 +19,18 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       id: params.id,
     },
   })
+
+  if (!barbershop) {
+    return notFound()
+  }
+
   return (
     <div>
       {/* Image */}
       <div className="relative h-[250px] w-full">
         <Image
-          src={barbershop?.imageUrl || "/placeholder.png"}
-          alt={barbershop?.name || "Barbearia"}
+          src={barbershop?.imageUrl}
+          alt={barbershop?.name}
           fill
           className="object-cover"
         />
@@ -49,7 +55,7 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
         </Button>
       </div>
 
-      <div className="border-b border-solid p-5">
+      <div className="gap-2 border-b border-solid p-5">
         <h1 className="mb-3 text-2xl font-bold">{barbershop?.name}</h1>
         <div className="mb-2 flex items-center gap-1">
           <MapIcon className="text-primary" size={18} />
@@ -60,6 +66,12 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           <StarIcon className="text-primary fill-primary" size={18} />
           <p className="text-sm">5,0 (499 avaliações)</p>
         </div>
+      </div>
+
+      {/*Description*/}
+      <div className="space-y-3 border-b border-solid p-5">
+        <h2 className="text-xs font-bold text-gray-400 uppercase">Sobre Nós</h2>
+        <p className="text-justify text-sm">{barbershop?.description}</p>
       </div>
     </div>
   )
